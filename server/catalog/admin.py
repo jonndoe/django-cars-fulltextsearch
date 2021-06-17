09e1e60c070c5a91d2a03e17f26baf4478be4a22
jonndoe <lukichevvasily@gmail.com>
2021-06-17 15:08:10 +0100

    added Car model

--- server/catalog/admin.py
+++ server/catalog/admin.py
@@ -1,3 +1,13 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Car
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    fields = ('id', 'country', 'description', 'points', 'price', 'variety', 'model',)
+    list_display = ('id', 'country', 'points', 'price', 'variety', 'model',)
+    list_filter = ('country', 'variety', 'model',)
+    ordering = ('variety',)
+    readonly_fields = ('id',)
