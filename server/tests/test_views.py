8b0e4aa88b22eae4b27e19b6e00b5eb0e6482388
jonndoe <lukichevvasily@gmail.com>
2021-06-19 08:59:29 +0100

    added db queries tests

--- server/tests/test_views.py
+++ server/tests/test_views.py
@@ -1,31 +1,50 @@
 from rest_framework.test import APIClient, APITestCase
 
 from catalog.models import Car
+from catalog.serializers import CarSerializer
 
 
 class ViewTests(APITestCase):
+    fixtures = ['test_cars.json']
+
+    def setUp(self):
+        self.client = APIClient()
+
     def test_empty_query_returns_everything(self):
-        car = Car.objects.create(
-            country="US",
-            description="A hardcore car",
-            points=90,
-            price=100.00,
-            variety="Sport coupe",
-            model="Ford Mustang",
-        )
-        client = APIClient()
-        response = client.get("/api/v1/catalog/cars/")
-        self.assertJSONEqual(
-            response.content,
-            [
-                {
-                    "country": "US",
-                    "description": "A hardcore car",
-                    "id": str(car.id),
-                    "points": 90,
-                    "price": "100.00",
-                    "variety": "Sport coupe",
-                    "model": "Ford Mustang",
-                }
-            ],
-        )
+        response = self.client.get("/api/v1/catalog/cars/")
+        cars = Car.objects.all()
+        self.assertJSONEqual(response.content, CarSerializer(cars, many=True).data)
+
+    def test_query_matches_variety(self):
+        response = self.client.get('/api/v1/catalog/cars/?query=sport')
+        self.assertEquals(1, len(response.data))
+        self.assertEquals("58ba903f-85ff-45c2-9bac-6d0732544841", response.data[0]['id'])
+
+    def test_query_matches_model(self):
+        response = self.client.get('/api/v1/catalog/cars/?query=Toyota')
+        self.assertEquals(1, len(response.data))
+        self.assertEquals("21e40285-cec8-417c-9a26-4f6748b7fa3a", response.data[0]['id'])
+
+    def test_query_matches_description(self):
+        response = self.client.get('/api/v1/catalog/cars/?query=car')
+        self.assertEquals(3, len(response.data))
+        self.assertCountEqual([
+            "58ba903f-85ff-45c2-9bac-6d0732544841",
+            "21e40285-cec8-417c-9a26-4f6748b7fa3a",
+            "0082f217-3300-405b-abc6-3adcbecffd67",
+        ], [item['id'] for item in response.data])
+
+    def test_can_filter_on_country(self):
+        response = self.client.get('/api/v1/catalog/cars/?country=France')
+        self.assertEquals(1, len(response.data))
+        self.assertEquals("0082f217-3300-405b-abc6-3adcbecffd67", response.data[0]['id'])
+
+    def test_can_filter_on_points(self):
+        response = self.client.get('/api/v1/catalog/cars/?points=87')
+        self.assertEquals(1, len(response.data))
+        self.assertEquals("21e40285-cec8-417c-9a26-4f6748b7fa3a", response.data[0]['id'])
+
+    def test_country_must_be_exact_match(self):
+        response = self.client.get('/api/v1/catalog/cars/?country=Frances')
+        self.assertEquals(0, len(response.data))
+        self.assertJSONEqual(response.content, [])
