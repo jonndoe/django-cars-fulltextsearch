b0a4e8518aa5fa05e5d82c656d8bd1809bde03cc
jonndoe <lukichevvasily@gmail.com>
2021-06-19 13:38:24 +0100

    refactor filters.py

--- server/catalog/filters.py
+++ server/catalog/filters.py
@@ -1,4 +1,5 @@
-from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
+from django.contrib.postgres.search import (SearchQuery, SearchRank,
+                                            SearchVector)
 from django.db.models import F, Q
 from django_filters.rest_framework import CharFilter, FilterSet
 
@@ -12,12 +13,7 @@ class CarFilterSet(FilterSet):
         search_query = Q(Q(search_vector=SearchQuery(value)))
         return (
             queryset.annotate(
-                search_vector=(
-                    SearchVector("variety", weight="A")
-                    + SearchVector("model", weight="A")
-                    + SearchVector("description", weight="B")
-                ),
-                search_rank=SearchRank(F("search_vector"), SearchQuery(value)),
+                search_rank=SearchRank(F("search_vector"), SearchQuery(value))
             )
             .filter(search_query)
             .order_by("-search_rank", "id")
