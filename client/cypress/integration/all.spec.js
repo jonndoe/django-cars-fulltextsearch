6b9504bfcd249e246f6de72917c671c915f4a287
jonndoe <lukichevvasily@gmail.com>
2021-06-20 14:38:00 +0100

    Add fixture to cy tests

--- client/cypress/integration/all.spec.js
+++ client/cypress/integration/all.spec.js
@@ -6,8 +6,11 @@ describe('Cars', function () {
 });
 
 it('Displays a list of results.', function () {
+  cy.intercept('GET', '**/api/v1/catalog/cars/**', { fixture: 'cars.json' }).as('getCars');
+
   cy.visit('/');
   cy.get('input#query').type('cabernet');
   cy.get('button').contains('Search').click();
+  cy.wait('@getCars');
   cy.get('div.card-title').should('contain', 'Cabernet Sauvignon');
 });
