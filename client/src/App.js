3a554bb97d736134c03e86e444586c13b1b896bc
jonndoe <lukichevvasily@gmail.com>
2021-06-20 12:33:22 +0100

    add react client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+import { Container } from 'react-bootstrap';
+
+function App () {
+  return (
+    <Container className='pt-3'>
+      <h1>Cars</h1>
+    </Container>
+  );
+}
+
+export default App;
