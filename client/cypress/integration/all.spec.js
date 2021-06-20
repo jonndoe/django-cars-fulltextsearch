37656b8fb193f13d97f10d4508d8e7784eac4608
jonndoe <lukichevvasily@gmail.com>
2021-06-20 14:20:45 +0100

    Add search tests

--- client/cypress/integration/all.spec.js
+++ client/cypress/integration/all.spec.js
@@ -4,3 +4,10 @@ describe('Cars', function () {
     cy.get('h1').should('contain', 'Cars');
   });
 });
+
+it('Displays a list of results.', function () {
+  cy.visit('/');
+  cy.get('input#query').type('cabernet');
+  cy.get('button').contains('Search').click();
+  cy.get('div.card-title').should('contain', 'Cabernet Sauvignon');
+});
--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+
+import ResultList from './components/ResultList';
+import Search from './components/Search';
+
 
 function App () {
+
+  const [results, setResults] = useState([]);
+
+  const search = query => {
+    setResults([
+      {
+        id: 'ecf8760a-d139-4d63-83bc-6ddbac9f87dc',
+        country: 'US',
+        description: 'A youthful, exciting car that offers plenty of earth and cassis.',
+        points: 92,
+        price: '65.00',
+        variety: 'Cabernet Sauvignon',
+        model: 'Staglin'
+      }
+    ]);
+  };
+
   return (
     <Container className='pt-3'>
       <h1>Cars</h1>
+      <p className='lead'>
+        Use the controls below to peruse the car catalog and filter the results.
+      </p>
+      <Row>
+        <Col lg={4}>
+          <Search search={search}/>
+        </Col>
+        <Col lg={8}>
+          <ResultList results={results}/>
+        </Col>
+      </Row>
     </Container>
   );
 }
