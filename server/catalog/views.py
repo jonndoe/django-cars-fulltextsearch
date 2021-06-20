d4f6567ad5095f4fea349f46881941f0b583018a
jonndoe <lukichevvasily@gmail.com>
2021-06-20 11:56:30 +0100

    Limit the number of results

--- server/catalog/views.py
+++ server/catalog/views.py
@@ -10,6 +10,9 @@ class CarsView(ListAPIView):
     serializer_class = CarSerializer
     filterset_class = CarFilterSet
 
+    def filter_queryset(self, request):
+        return super().filter_queryset(request)[:100]
+
 
 class CarSearchWordsView(ListAPIView):
     queryset = CarSearchWord.objects.all()
