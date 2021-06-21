6d192fbfb822ef4e329c03f41087c8fdb45ec8a4
jonndoe <lukichevvasily@gmail.com>
2021-06-21 08:56:58 +0100

    Changed files mood

--- client/cypress/integration/all.spec.js
+++ client/cypress/integration/all.spec.js
@@ -9,6 +9,8 @@ it('Displays a list of results.', function () {
   cy.intercept('GET', '**/api/v1/catalog/cars/**', { fixture: 'cars.json' }).as('getCars');
 
   cy.visit('/');
+  cy.get('input#country').type('US');
+  cy.get('input#points').type('92');
   cy.get('input#query').type('cabernet');
   cy.get('button').contains('Search').click();
   cy.wait('@getCars');
